ec69efdf58591319c4989941035387cb4eaa8367
brenobaptista <brenomb07@hotmail.com>
2020-01-20 16:44:48 -0300

    PoW and Transactions

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # acens-blockchain-nodejs
-AcensCoin - A cryptocurrency made using Node.js
+AcensCoin - A blockchain made using Node.js
+
+# Credits
+Thanks to [Savjee's video tutorials!](https://www.youtube.com/playlist?list=PLzvRQMJ9HDiTqZmbtFisdXFxul5k0F-Q4)
+PS.: A few tweaks must be made in his tutorials if you wanna implement it corrently (cough cough bugs cough).
---
+++ src/main.js
@@ -0,0 +1,24 @@
+const { Blockchain, Transaction } = require("./blockchain");
+const EC = require("elliptic").ec;
+const ec = new EC("secp256k1");
+
+const myKey = ec.keyFromPrivate(
+  "699f1aa79a923386040bc5c5f7fe2c2ef5e4f5d03720cbc0a72c428faa7d9567"
+);
+const myWalletAddress = myKey.getPublic("hex");
+
+let acensCoin = new Blockchain();
+
+const tx1 = new Transaction(myWalletAddress, "public key goes here", 10);
+tx1.signTransaction(myKey);
+acensCoin.addTransaction(tx1);
+
+console.log("Starting the miner...");
+acensCoin.minePendingTransactions(myWalletAddress);
+
+console.log(
+  "Balance of breno is ",
+  acensCoin.getBalanceOfAddress(myWalletAddress)
+);
+
+console.log("Is chain valid? ", acensCoin.isChainValid() ? 'Yes' : 'No');
